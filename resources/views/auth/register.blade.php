c6257200e10fab5e97194e5f343bc6982329807f
AlanMendoza09 <37988489+AlanMendoza09@users.noreply.github.com>
2021-03-05 21:36:26 -0600

    Commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,12 +29,14 @@
         <label for="">Password Validation</label>
         <input type="password" name="password_confirmation">
     </div>
+    <div>
         <select name="role" id="">
             <option value="guest">Guest</option>
             <option value="user">User</option>
             <option value="admin">Admin</option>
             <option value="super">Super Admin</option>
         </select>
+    <div>
     <div>
         <input type="submit" value="Register">
     </div>
